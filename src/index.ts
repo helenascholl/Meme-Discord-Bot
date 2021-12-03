bf23d2235df1106d117f70a5db986fc7b6fd61cc
schollsebastian <scholl.sebastian@gmx.at>
2021-12-03 08:46:34 +0100

    Import and parse config

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import Discord from 'discord.js';
 import dotenv from 'dotenv';
+import MemeConfig from './meme-config.interface';
+import config from './config.json';
 
 dotenv.config();
 
@@ -7,9 +9,14 @@ const token = process.env['NODE_ENV'] === 'development' ?
   process.env['DISCORD_DEV_TOKEN'] :
   process.env['DISCORD_PROD_TOKEN'];
 const client = new Discord.Client();
+const memes = new Map<string, MemeConfig>();
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user!.tag}`);
+
+  for (const meme of config as MemeConfig[]) {
+    memes.set(meme.name, meme);
+  }
 });
 
 client.on('message', message => {
