92c980407723cdd94448344e57eec81008ce1222
schollsebastian <scholl.sebastian@gmx.at>
2021-12-03 15:50:38 +0100

    Add command to add memes

--- src/index.ts
+++ src/index.ts
@@ -25,15 +25,17 @@ client.on('ready', () => {
     console.log('Created config.json');
   }
 
-  const config: MemeConfig[] = JSON.parse(fs.readFileSync('./src/config.json', 'utf8'));
-
-  for (const meme of config) {
-    memes.set(meme.name, meme);
-  }
+  initMemes();
 });
 
 client.on('message', async message => {
-  if (message.content.startsWith('!say')) {
+  if (message.content.startsWith('!say config')) {
+    const text = message.content.indexOf(' ', 10) != -1
+      ? message.content.substring(message.content.indexOf(' ', 10) + 1)
+      : '';
+
+    config(text, message);
+  } else if (message.content.startsWith('!say')) {
     const memeName = message.content.split(' ')[1];
     const text = message.content.indexOf(' ', 5) !== -1
       ? message.content.substring(message.content.indexOf(' ', 6) + 1)
@@ -64,6 +66,115 @@ client.on('message', async message => {
 });
 
 client.login(token).catch(console.error);
+
+function config(text: string, message: Discord.Message): void {
+  if (!message.member?.hasPermission('ADMINISTRATOR')) {
+    message.channel.send("You don't have permission to do that!")
+      .catch(console.error);
+  } else {
+    const commandText = text.indexOf(' ') !== -1
+      ? text.substring(text.indexOf(' ') + 1)
+      : '';
+
+    if (text.startsWith('add')) {
+      addMeme(commandText, message);
+    } else {
+      message.channel.send('Invalid operation!')
+        .catch(console.error);
+    }
+  }
+}
+
+async function addMeme(text: string, message: Discord.Message): Promise<void> {
+  if (text !== '') {
+    const splitText = text.split(' ');
+    const memeName = splitText[0];
+    let imageUrl = message.attachments.size > 0 ? undefined : splitText[1];
+    const customPrefix = message.attachments.size > 0 ? splitText[1] : splitText[2];
+
+    if (Array.from(memes.values()).map(m => m.name).includes(memeName) || memeName === 'config') {
+      message.channel.send('That meme already exists!')
+        .catch(console.error);
+    } else if (Array.from(memes.values())
+      .filter(m => m.customPrefix !== undefined)
+      .map(m => m.customPrefix)
+      .includes(customPrefix)) {
+      message.channel.send('That prefix is already in use!')
+        .catch(console.error);
+    } else {
+      if (message.attachments.size > 0) {
+        const attachment = Array.from(message.attachments.values())
+          .find(a => [ 'png', 'jpg', 'jpeg' ].includes(a.url.split('.').pop() ?? ''));
+
+        if (attachment) {
+          imageUrl = attachment.url;
+        } else {
+          message.channel.send('Unsupported image type!')
+            .catch(console.error);
+        }
+      } else if (imageUrl !== undefined && imageUrl !== '') {
+        try {
+          new URL(imageUrl);
+        } catch (e) {
+          message.channel.send('Invalid URL!')
+            .catch(console.error);
+        }
+      } else {
+        message.channel.send('No image specified!')
+          .catch(console.error);
+      }
+
+      if (imageUrl !== undefined) {
+        try {
+          await saveImage(imageUrl, memeName);
+        } catch (e) {
+          message.channel.send('Unsupported image type!')
+            .catch(console.error);
+        }
+
+        const config: MemeConfig[] = Array.from(memes.values());
+        config.push({
+          name: memeName,
+          filename: `${memeName}.png`,
+          customPrefix: customPrefix
+        });
+
+        await saveConfig(config);
+        await initMemes();
+
+        message.channel.send(`Added meme ${splitText[0]}!`)
+          .catch(console.error);
+      }
+    }
+  } else {
+    message.channel.send('Please specify a name!')
+      .catch(console.error);
+  }
+}
+
+async function saveConfig(config: MemeConfig[]): Promise<void> {
+  fs.writeFileSync('./src/config.json', JSON.stringify(config, null, 2));
+}
+
+async function initMemes(): Promise<void> {
+  const config: MemeConfig[] = JSON.parse(fs.readFileSync('./src/config.json', 'utf8'));
+
+  for (const meme of config) {
+    memes.set(meme.name, meme);
+  }
+}
+
+async function saveImage(url: string, filename: string): Promise<void> {
+  const image = await loadImage(url);
+  const imageHeight = image.height * IMAGE_WIDTH / image.width;
+
+  const canvas = createCanvas(IMAGE_WIDTH, imageHeight);
+  const ctx = canvas.getContext('2d');
+
+  ctx.drawImage(image, 0, 0, IMAGE_WIDTH, imageHeight);
+
+  fs.writeFileSync(`images/${filename}.png`, canvas.toBuffer());
+}
 
 async function sendMeme(meme: MemeConfig | string, text: string, message: Discord.Message): Promise<void> {
   if (text !== '') {
