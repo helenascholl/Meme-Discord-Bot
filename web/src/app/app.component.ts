fd3a5816e50dbc4439de7d49913443d1c3a155cf
schollsebastian <scholl.sebastian@gmx.at>
2021-12-16 13:42:41 +0100

    Import and cast config

--- web/src/app/app.component.ts
+++ web/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import config from '../assets/config.json';
+import { Config } from './config';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,7 @@ import { Component } from '@angular/core';
   styleUrls: [ './app.component.scss' ]
 })
 export class AppComponent {
-  title = 'meme-bot-web';
+
+  public readonly config = config as Config[];
+
 }
