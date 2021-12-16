ab61b6a3d209e7edd72de9dece046df6eb7c4cff
schollsebastian <scholl.sebastian@gmx.at>
2021-12-16 13:28:59 +0100

    Satisfy linter rules

--- web/src/app/meme/meme.component.ts
+++ web/src/app/meme/meme.component.ts
@@ -3,7 +3,7 @@ import { Component, Input, OnInit } from '@angular/core';
 @Component({
   selector: 'app-meme',
   templateUrl: './meme.component.html',
-  styleUrls: ['./meme.component.scss']
+  styleUrls: [ './meme.component.scss' ]
 })
 export class MemeComponent {
 
