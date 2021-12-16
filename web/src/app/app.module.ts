80bbd84a1b14c8253913cdc5e83c303c7286198e
schollsebastian <scholl.sebastian@gmx.at>
2021-12-16 11:19:43 +0100

    Add input values to MemeComponent

--- web/src/app/app.module.ts
+++ web/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { MemeComponent } from './meme/meme.component';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { MatCardModule } from '@angular/material/card';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import { NoopAnimationsModule } from '@angular/platform-browser/animations';
   ],
   imports: [
     BrowserModule,
-    NoopAnimationsModule
+    NoopAnimationsModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- web/src/app/meme/meme.component.ts
+++ web/src/app/meme/meme.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-meme',
   templateUrl: './meme.component.html',
   styleUrls: ['./meme.component.scss']
 })
-export class MemeComponent implements OnInit {
+export class MemeComponent {
+
+  @Input()
+  public imagePath!: string;
+
+  @Input()
+  public meme!: string;
+
+  @Input()
+  public customPrefix: string | undefined;
 
   constructor() { }
-
-  ngOnInit(): void {
-  }
 
 }
