b2642d5eca6f7984b02fbdb963e1c9ed0b629b3e
schollsebastian <scholl.sebastian@gmx.at>
2021-12-16 18:52:06 +0100

    Use ConfigService in AppComponent

--- web/src/app/app.component.ts
+++ web/src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
-import config from '../assets/config.json';
-import { Config } from './config';
+import { ConfigService } from './config.service';
 
 @Component({
   selector: 'app-root',
@@ -9,7 +8,10 @@ import { Config } from './config';
 })
 export class AppComponent {
 
-  public readonly config = config as Config[];
-  public readonly inviteLink = 'https://discordapp.com/oauth2/authorize?&client_id=916227104666968074&scope=bot&permissions=34816';
+  public readonly inviteLink: string;
+
+  constructor(public configService: ConfigService) {
+    this.inviteLink = 'https://discordapp.com/oauth2/authorize?&client_id=916227104666968074&scope=bot&permissions=34816';
+  }
 
 }
