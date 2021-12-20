7a0a137800cc7d59381de83696a72d3ca70ea420
schollsebastian <scholl.sebastian@gmx.at>
2021-12-20 11:43:25 +0100

    Use location.origin for baseUrl

--- web/src/app/config.service.ts
+++ web/src/app/config.service.ts
@@ -2,7 +2,6 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Config } from './config';
 import { Observable } from 'rxjs';
-import { environment } from '../environments/environment';
 
 @Injectable({
   providedIn: 'root'
@@ -10,19 +9,14 @@ import { environment } from '../environments/environment';
 export class ConfigService {
 
   public config: Config[];
-  private readonly baseUrl: string;
 
   constructor(private http: HttpClient) {
     this.config = [];
-    this.baseUrl = environment.production
-      ? 'http://127.0.0.1'
-      : 'http://localhost:4200';
-
     this.updateConfig();
   }
 
   public updateConfig(): Observable<Config[]> {
-    const request = this.http.get<Config[]>(`${this.baseUrl}/assets/config.json`);
+    const request = this.http.get<Config[]>(`${location.origin}/assets/config.json`);
     request.subscribe(c => this.config = c);
 
     return request;
